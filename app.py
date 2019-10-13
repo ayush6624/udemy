84134f8c15e7d8752017ba143628b04b86a8d810
ayush6624 <ayushg1214@gmail.com>
2019-10-13 16:12:15 +0530

    major changes to backend

--- app.py
+++ app.py
@@ -1,46 +1,67 @@
-import os
-from flask import Flask, render_template, url_for, send_from_directory, request, session, redirect
-import pymongo
+from functools import wraps
+import sys
 import bcrypt
+import pymongo
+import os
+from flask import Flask, render_template, url_for, send_from_directory, \
+    request, session, redirect, flash
 
 app = Flask(__name__)
-# app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 a = os.environ.get("MONGO_URI")
+b = os.environ.get("FLASK_SECRET_KEY")
 mongo = pymongo.MongoClient(a)
-app.secret_key = "ayushgoyal"
+app.secret_key = b  # Use Env variable
+
+
+# Use @login_required to use that stuff
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'email' in session:
+            return f(*args, **kwargs)
+        else:
+            flash("Login First!")
+            return(redirect("/accounts"))
+    return wrap
+
+
+@app.route('/secret')
+@login_required
+def secret():
+    return session['email']
 
 
 @app.route('/')
 def index():
-    ##
-    if 'email' in session:
-        return "you are logged in as " + session['email']
-    ##
+    '''Returns the homepage'''
     return render_template('index.html')
 
 
 @app.route('/assets/<path:path>')
 def css(path):
+    '''Serve static content for the homepage'''
     return send_from_directory('assets', path)
 
 
 @app.route('/<course>/<submodule>/<course_num>')
 def find_corese(course, submodule, course_num):
+    '''Finds the course'''
     return render_template('course.html', course=course, submodule=submodule, course_num=course_num)
 
 # Account management
 @app.route('/accounts')
 def show_login():
+    '''Show login page'''
     return render_template("accounts.html")
 
 
 @app.route('/accounts/<action>', methods=['POST', 'GET'])
 def login(action):
+    '''Login API Endpoint using POST'''
     if(action == "login"):
         if request.method == 'POST':
             email = request.form["login"]  # use #name value here
             password = request.form["password"]
-            # s = (f"The email {email} is successfully registered with us. Password{password}")
             users = mongo.udemy.users
             login_user = users.find_one({"email": email})
 
@@ -51,10 +72,21 @@ def login(action):
                     return redirect('/')
                 return "Invalid username/password combination"
             return "invalid username"
+        elif request.method == 'GET':
+            print(1 + 1)
+            return render_template('accounts.html')
     if (action == "signup"):
         if request.method == 'POST':
             email = request.form["signup_email"]  # use #name value here
             return render_template("signup.html", email=email)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.clear()
+    flash("Successfully Logged out")
+    return redirect(url_for('index'))
 
 
 @app.route('/accounts/signup/new', methods=['POST', 'GET'])
