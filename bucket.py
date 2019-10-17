f85400a7b75ca632fae34a975fb20e310d008c7c
ayush6624 <ayushg1214@gmail.com>
2019-10-17 17:22:12 +0530

    Added signed urls and modified bucket acl

--- bucket.py
+++ bucket.py
@@ -2,7 +2,6 @@ from minio import Minio
 from minio.error import ResponseError, BucketNotEmpty
 import os
 import json
-import boto3
 from datetime import datetime, timedelta
 
 s3_access_key = os.environ.get("S3_ACCESS_KEY")
@@ -12,9 +11,3 @@ s3_secret_key = os.environ.get("S3_SECRET_KEY")
 client = Minio('s3.goyal.club',
                access_key=s3_access_key,
                secret_key=s3_secret_key, secure=True)
-
-try:
-    print(client.presigned_get_object('static', 'mongodb/01/001.mp4', expires=timedelta(minutes=2)))
-
-except ResponseError as err:
-    print(err)
