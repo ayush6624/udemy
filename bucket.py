4498abdde29c3acdf37ab57cf0540f79ffff7851
Ayush Goyal <ayushg1214@gmail.com>
2020-01-02 01:27:00 +0530

    Fix: changed domain name to ayushgoyal.dev

--- bucket.py
+++ bucket.py
@@ -8,6 +8,6 @@ s3_access_key = os.environ.get("S3_ACCESS_KEY")
 s3_secret_key = os.environ.get("S3_SECRET_KEY")
 
 
-client = Minio('s3.goyal.club',
+client = Minio('s3.ayushgoyal.dev',
                access_key=s3_access_key,
                secret_key=s3_secret_key, secure=True)
